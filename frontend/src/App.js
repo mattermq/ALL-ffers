effefa13a944ef4df5edc8f3d4a95435d719961a
Waldemar Kunz <w_kunz@web.de>
2020-08-07 09:44:51 +0200

    signup, login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,25 +2,70 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './app/store.js'
 import './App.css';
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 import FirstPage from './components/FirstPage';
+import Signup from './components/Signup.jsx';
+import Login from './components/Login.jsx';
 
 console.log(store.getState())
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="app_wrapper">
-        <div className="header"> <Header /> </div>
-     {/* <div className="firstPage"> <FirstPage /> </div> */}
-        <div className="wrap_main">
-          <div className="main"> <Main /> </div>
-        </div>
-        <div className="footer"> <Footer /> </div>
-      </div>
+      <Router>
+        {/* <div className="firstPage"> <FirstPage /> </div> */}
+
+        <Switch>
+
+          <Route exact path="/">
+            <div className="header"> <Header /> </div>
+            <div className="app_wrapper">
+              <div className="wrap_main">
+                <div className="main"> <Main /> </div>
+              </div>
+            </div>
+            <div className="footer"> <Footer /> </div>
+          </Route>
+
+          <Route exact path="/main">
+            <div className="header"> <Header /> </div>
+            <div className="app_wrapper">
+              <div className="wrap_main">
+                <div className="main"> <Main /> </div>
+              </div>
+            </div>
+            <div className="footer"> <Footer /> </div>
+          </Route>
+
+
+          <Route exact path="/signup">
+            <div className="header"> <Header /> </div>
+            <div className="app_wrapper">
+              <div className="wrap_main">
+                <div className="main"> <Signup /></div>
+              </div>
+            </div>
+            <div className="footer"> <Footer /> </div>
+          </Route>
+
+          <Route exact path="/login">
+            <div className="header"> <Header /> </div>
+            <div className="app_wrapper">
+              <div className="wrap_main">
+                <div className="main"><Login /></div>
+              </div>
+            </div>
+            <div className="footer"> <Footer /> </div>
+          </Route>
+
+        </Switch>
+
+      </Router>
+
     </Provider>
   );
 }
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import axios from 'axios';
+
+const axiosQ = axios.create({ withCredentials: true });
+
+
+export default function Login() {
+
+  const dispatch = useDispatch()
+  const initialState = {
+    email: '',
+    password: '',
+  };
+  const [state, setState] = useState(initialState)
+
+  const changeHandler = (e) => {
+    const { name, value } = e.target
+    setState({ ...state, [name]: value })
+  }
+
+  const submitHandler = async (e) => {
+    e.preventDefault()
+    const response = await axiosQ.post('http://localhost:3003/login', state)
+    console.log(response)
+    setState(initialState)
+    // if (response.status === 200)
+    //   dispatch(userLoginAC(response.data))
+    // history.push('/')
+  }
+
+  return (
+    <form onSubmit={submitHandler}>
+
+      <label htmlFor="email">Емайл</label>
+      <input type="email" name="email" required onChange={changeHandler} value={state.email} />
+
+      <label htmlFor="password">Пароль</label>
+      <input type="password" name="password" required onChange={changeHandler} value={state.password} />
+
+      <button type="submit">Зарегистрироваться</button>
+    </form>
+  )
+}
