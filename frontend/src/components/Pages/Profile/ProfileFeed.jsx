665c31199e71ab995846cf21f4130f46a8f00013
anton-iavorskii <89117058000@mail.ru>
2020-08-10 17:56:05 +0300

    anton

--- frontend/src/components/Pages/Profile/ProfileFeed.jsx
+++ frontend/src/components/Pages/Profile/ProfileFeed.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import CardShort from '../../CardShort';
-import CardNormal from '../../CardNormal';
+import CardNormalP from '../../CardNormalP';
 import CardExtended from '../../CardExtended';
 import Pagination from '../../Pagination'
 import { useSelector, useDispatch } from 'react-redux';
@@ -31,6 +31,14 @@ export default function ProfileFeed() {
   if (paginatedOffers)
     return (
       <>
+      <div className="wrap_profile">
+        <div className="prof_blockCards">
+          <div className="blockCards_tabs">
+            <div className="tab">В избранном</div>
+            <div className="tab">В работе</div>
+            <div className="tab">Сделано</div>
+          </div>
+          <div className="blockCards_cards">
         {
           paginatedOffers.length > 0 && paginatedOffers.map(offer => {
             if (offer.hasExpandedSize === true)
@@ -38,11 +46,16 @@ export default function ProfileFeed() {
             else if (componentsSize === 1)
               return <CardShort key={offer._id} offer={offer} />
             else
-              return <CardNormal key={offer._id} offer={offer} />
+              return <CardNormalP key={offer._id} offer={offer} />
           })
         }
-
         <Pagination totalPosts={offers.length} />
+        </div>
+        </div>
+        <div className="prof_blockStat">
+          <div className="blockStat_graf">Статистика</div> 
+        </div>
+      </div>       
       </>
     )
   else return null
