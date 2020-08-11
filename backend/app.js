a18cabf551f6084a4ae76e5d110ec173fcd6a57f
re-mark <a_hl@mail.ru>
2020-08-11 16:42:25 +1000

    Merge master

--- backend/app.js
+++ backend/app.js
@@ -96,7 +96,6 @@ app.post('/logout', function (req, res) {
   res.json({ message: 'logout succesful' })
 })
 
-
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((user, done) => done(null, user));
 
